42de01e752e18196729b3619b85240adf4153e91
Matt-Tanti <66307285+Matt-Tanti@users.noreply.github.com>
2024-07-29 20:56:29 +0200

    Fixed loading css

--- src/views/Common/Loading.tsx
+++ src/views/Common/Loading.tsx
@@ -11,25 +11,24 @@ const styles: Styles = {
 };
 
 const Spinner = styled.span`
-    width: 3rem;
-    height: 3rem;
-    margin: 3rem;
-    border: 5px solid #FFF;
-    border-bottom-color: transparent;
-    border-radius: 50%;
-    display: inline-block;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-    }
+  width: 3rem;
+  height: 3rem;
+  margin: 3rem;
+  border: 5px solid #fff;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
 
-    @keyframes rotation {
+  @keyframes rotation {
     0% {
-        transform: rotate(0deg);
+      transform: rotate(0deg);
     }
     100% {
-        transform: rotate(360deg);
+      transform: rotate(360deg);
     }
-    }
+  }
 `;
 
 const Loading = () => {
